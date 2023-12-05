e4e4a408cd0bb2f94ed62889e453534498943a07
Mayank-Arya <ma8183468@gmail.com>
2023-12-05 17:26:26 +0530

    socket.io server is running

--- backend/controllers/doubtController.js
+++ backend/controllers/doubtController.js
@@ -4,6 +4,28 @@ const User = require('../model/users');
 const TutorAvailability = require('../model/tutorAvailability');
 const DoubtRequest = require('../model/doubtRequest');
 
+const http = require('http')
+const socketIo = require('socket.io')
+
+const server = http.createServer()
+const io = socketIo(server)
+
+const connectedTutors = new Set()
+
+io.on('connection',(socket) => {
+    console.log('New Connection', socket.id)
+})
+
+socket.on('tutorConnect', (tutorId) => {
+    console.log("Tutor connected:", tutorId)
+    connnectTutor.add(tutorId)
+})
+
+socket.on('disconnect', () => {
+    console.log("User disconnected",socket.id)
+    connectedTutors.delete(socket.io)
+})
+
 const findEligibleTutors = async (studentId, doubtSubject) => {
   try {
     // Find online tutors matching the student's criteria
@@ -25,7 +47,12 @@ const findEligibleTutors = async (studentId, doubtSubject) => {
       },
     ]);
 
-    // Notify eligible tutors (you can implement your notification mechanism here)
+    eligibleTutors.forEach((tutor) => {
+        const tutorSocket = io.sockets.sockets.get(tutor.tutorId.toString())
+        if(tutorSocket){
+            tutorSocket.emit('newDoubtRequest', {studentId,doubtSubject})
+        }
+    })
 
     // Create a doubt request record
     await DoubtRequest.create({ studentId, doubtSubject });
@@ -37,4 +64,4 @@ const findEligibleTutors = async (studentId, doubtSubject) => {
   }
 };
 
-module.exports = { findEligibleTutors };
+module.exports = { findEligibleTutors, server };
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const {connection} = require('./config/db')
 const {userRouter} = require('./routes/user.routes')
+const http = require('http')
+const socketIo = require('socket.io')
 const {doubtRouter} = require('./routes/doubtRoutes.routes')
 const app = express()
 app.use(express.json())
@@ -12,6 +14,9 @@ app.get("/",(req,res)=>{
 
 app.use('/user',userRouter)
 app.use('/doubt',doubtRouter)
+
+const server = http.createServer(app)
+const io = socketIo(server)
 
 app.listen(9090, async () => {
     try{
